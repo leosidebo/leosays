a93307091adf41b10c31064e13429425404aa443
Leo Sidebo <abletoirper@gmail.com>
2019-10-25 22:13:55 +0200

    Styling for body and box class

--- main.js
+++ main.js
@@ -1,26 +1,48 @@
 let boxes = [];
 boxes = document.getElementsByClassName('box');
-
-console.log(boxes);
-shuffle(boxes);
 console.log(boxes);
 
 
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-  
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-  
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-  
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
+window.onload = addColor();
+
+/**
+ * Adds color to each box.
+ */
+function addColor() 
+{
+    shuffle(boxes);
+    console.log(boxes);
+    for(let i=0; i < boxes.length; i++)
+    {
+        if (i == 0) {
+            boxes[i].classList.add('red');
+        }
+
+        if (i == 1) {
+            boxes[i].classList.add('blue');
+        }
+
+        if (i == 2) {
+            boxes[i].classList.add('green');
+        }
+
+        if (i == 3) {
+            boxes[i].classList.add('yellow');
+        }
     }
-  
-    return array;
-  }
+}
+
+/**
+ * Randomize array element order in-place.
+ * Using Durstenfeld shuffle algorithm.
+ */
+function shuffle(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    console.log(array);
+}
