633a2fc892fd8fe7ab603001f9b00e388c6f5191
leosidebo <abletoirper@gmail.com>
2019-11-15 15:29:33 +0100

    Attempting to fix bug on two colors lighting up in a row

--- main.js
+++ main.js
@@ -1,63 +1,61 @@
 let boxes = [];
-boxes = document.getElementsByClassName('box');
+boxes = document.getElementsByClassName("box");
 let gameOrder = [];
 let currentLoop = 0;
 let gameLength = 1;
-
 
 window.onload = addColor();
 
 /**
  * Adds color to each box.
  */
-function addColor() 
-{
-    shuffle(boxes);
-    for(let i=0; i < boxes.length; i++)
-    {
-        if (i == 0) {
-            boxes[i].classList.add('red');
-        }
-
-        if (i == 1) {
-            boxes[i].classList.add('blue');
-        }
-
-        if (i == 2) {
-            boxes[i].classList.add('green');
-        }
-
-        if (i == 3) {
-            boxes[i].classList.add('yellow');
-        }
+function addColor() {
+  shuffle(boxes);
+  for (let i = 0; i < boxes.length; i++) {
+    if (i == 0) {
+      boxes[i].classList.add("red");
     }
+
+    if (i == 1) {
+      boxes[i].classList.add("blue");
+    }
+
+    if (i == 2) {
+      boxes[i].classList.add("green");
+    }
+
+    if (i == 3) {
+      boxes[i].classList.add("yellow");
+    }
+  }
 }
 
 function mainGame() {
+  currentLoop = 0;
 
-    currentLoop = 0;
+  let mainLoop = setInterval(function() {
+    console.log("ree");
 
-    let mainLoop = setInterval(function() {
-        console.log('ree');
+    if (gameLength > 1 && currentLoop == 0) {
+      boxes[gameOrder[currentLoop]].classList.remove("active");
+    } else if (gameLength > 1) {
+      boxes[gameOrder[currentLoop - 1]].classList.remove("active");
+    }
 
-        if (currentLoop > 0) {
-            boxes[gameOrder[currentLoop - 1]].classList.remove('active');
-        }
+    gameOrder.push(Math.floor(Math.random() * 4));
+    console.log(boxes[gameOrder[currentLoop]].classList);
+    boxes[gameOrder[currentLoop]].classList.add("active");
+    console.log(boxes[gameOrder[currentLoop]]);
+    currentLoop++;
 
-        gameOrder.push(Math.floor(Math.random() * 4))
-        boxes[gameOrder[currentLoop]].classList.add('active');
-        currentLoop++;
+    if (currentLoop == gameLength) {
+      console.log("yee");
+      clearInterval(mainLoop);
+      gameLength++;
+    }
+  }, 2000);
 
-        if (currentLoop == gameLength) {
-            console.log('yee');
-            clearInterval(mainLoop);
-            gameLength++;
-        }
-
-    }, 2000);
-
-    // for(let i = 0; i < gameOrder.length; i++) {}
-
+  // for(let i = 0; i < gameOrder.length; i++) {}
 }
 
 /**
@@ -65,11 +63,11 @@ function mainGame() {
  * Using Durstenfeld shuffle algorithm.
  */
 function shuffle(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-
+  // Kan troligen fixa denna genom att lägga de nya resultaten i en annan array
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
 }
